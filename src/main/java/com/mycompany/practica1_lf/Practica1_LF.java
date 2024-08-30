eeb4051e65b402af8574dd3f22356dbcea7498db
Brandon8A <brandonochoa202030023>
2024-08-30 13:05:36 -0600

    Agregando primeras clases java

---
+++ src/main/java/com/mycompany/practica1_lf/Practica1_LF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1_lf;
+
+import com.mycompany.practica1_lf.FrontEnd.FrameAnalizadorLexico;
+
+/**
+ *
+ * @author brandon
+ */
+public class Practica1_LF {
+
+    public static void main(String[] args) {
+        FrameAnalizadorLexico frameAnalizadorLexico = new FrameAnalizadorLexico();
+        frameAnalizadorLexico.setLocationRelativeTo(null);
+        frameAnalizadorLexico.setVisible(true);
+    }
+}
